d3e0e85aae062aee875028eb00e5674ccdcd8685
Joseph Mallari <jomallari98@gmail.com>
2025-02-03 05:15:40 -0600

    Fixed merge conflicts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  application:
+  {
+    name: 'angular-starter',
+    version: 'Angular 15.1.5',
+    bootstrap: 'Bootstrap 5.2.3',
+    fontawesome: 'Font Awesome 6.3.0',
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  application:
+  {
+    name: 'angular-bootstrap',
+    version: 'Angular 15.1.5',
+    bootstrap: 'Bootstrap 5.2.3',
+    fontawesome: 'Font Awesome 6.3.0',
+  }
+};
